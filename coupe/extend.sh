02f4d81ba7a743b25be7ba3b917ed7f0600b78ba
detrito <detrito@inventati.org>
2017-11-12 20:11:25 +0100

    Coupe Marguerite Bayes

--- coupe/extend.sh
+++ coupe/extend.sh
@@ -3,3 +3,4 @@
 
 extend  --specfile specfile_main.txt ../main.3d coupe_main.3d
 extend  --specfile specfile_st_barbe.txt ../main.3d coupe_st_barbe.3d
+extend  --specfile specfile_marguerite_bayes.txt ../main.3d coupe_marguerite_bayes.3d
