a3692ec87686d1fd160157c74028899d6c322b93
detrito <detrito@inventati.org>
2017-12-01 21:20:18 +0100

    Coupes Canicule et Ventilateur

--- coupe/extend.sh
+++ coupe/extend.sh
@@ -5,3 +5,4 @@ extend  --specfile specfile_main.txt ../main.3d coupe_main.3d
 extend  --specfile specfile_st_barbe.txt ../main.3d coupe_st_barbe.3d
 extend  --specfile specfile_marguerite_bayes.txt ../main.3d coupe_marguerite_bayes.3d
 extend  --specfile specfile_cigale.txt ../main.3d coupe_cigale.3d
+extend  --specfile specfile_ventilateur.txt ../main.3d coupe_ventilateur.3d
